6c33e9d30d4bdb11a3b54bd02c1b66b6728c2482
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-09-10 11:13:01 +0200

    Week 1 opdracht 1

--- Week 1/Opdracht 1.sql
+++ Week 1/Opdracht 1.sql
@@ -1,2 +1,11 @@
 -- Er is een lijst nodig met alle patiënten uit het postcodegebied 6202 JK.
 -- De resultatentabel moet de achternaam, voornaam en postcode tonen.
+
+SELECT
+  patient.patient_achternaam,
+  patient.patient_voornaam,
+  patient.patient_postcode
+FROM
+  patient
+WHERE
+  patient.patient_postcode = '6202 JH';
