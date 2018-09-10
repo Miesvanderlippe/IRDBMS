5084c1350eb0ffe51df07a4ccbcb714d8b43de61
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-09-10 11:19:36 +0200

    Week 1 opdracht 2

--- Week 1/Opdracht 2.sql
+++ Week 1/Opdracht 2.sql
@@ -1,2 +1,10 @@
 -- Iemand van Personeelszaken heeft een query nodig die het aantal medewerkers van het ziekenhuis telt.
 -- Geef de kolommen zinvolle namen
+
+SELECT
+  personeel.pers_kantoor_locatie,
+  count(*) as Aantal_op_locatie
+FROM
+  personeel
+GROUP BY
+  personeel.pers_kantoor_locatie
