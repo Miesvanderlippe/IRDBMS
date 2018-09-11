9a921ed4a14a251c632d88a51ec1e972a970185c
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 17:03:34 +0200

    Week 1 opdracht 14

--- Week 1/Opdracht 14.sql
+++ Week 1/Opdracht 14.sql
@@ -1,1 +1,8 @@
 -- Pas de query bij 13 aan, zodat de patiënten getoond worden die nog niet aan een bednummer gekoppeld zijn.
+
+SELECT
+  patient.patient_voornaam
+FROM
+  patient
+WHERE
+  patient.patient_bed_nr ISNULL
