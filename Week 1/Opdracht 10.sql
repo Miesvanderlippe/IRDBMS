1f967b564e80e541efc4a3ce7d43cabdc9a6b284
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-09-11 14:00:22 +0200

    Week 1 opdracht 10

--- Week 1/Opdracht 10.sql
+++ Week 1/Opdracht 10.sql
@@ -1,2 +1,11 @@
 -- Maak een select statement om te zien welke records in de patiënt tabel in aanmerking komen om
 -- gearchiveerd te worden.
+
+SElECT
+  patient.patient_voornaam,
+  patient.patient_achternaam,
+  patient.patient_nr
+FROM
+  patient
+WHERE
+  patient.patient_bed_nr ISNULL
