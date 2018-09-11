8ccdd842e4398b6ac9059a3e8b52db93ba6892e1
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 16:49:05 +0200

    Week 1 opdracht 13

--- Week 1/Opdracht 13.sql
+++ Week 1/Opdracht 13.sql
@@ -1,2 +1,15 @@
 -- Maak een query die een lijst toont van alle patiënten die zijn toegewezen aan bed.
 -- De resultatentabel moet de naam, het bednummer en het bedtype bevatten.
+
+SELECT
+  patient.patient_voornaam,
+  patient.patient_bed_nr,
+  bt.bed_type_omschrijving
+FROM
+  patient
+LEFT JOIN
+  bed b on patient.patient_bed_nr = b.bed_nr
+LEFT JOIN
+  bed_type bt on b.bed_type_nr = bt.bed_type_nr
+WHERE
+  patient.patient_bed_nr NOTNULL
