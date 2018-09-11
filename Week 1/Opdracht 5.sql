3b83ed79d9d80b05a8e1c6500d04a7bdac4617f2
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-09-11 11:38:02 +0200

    Week 1 opdracht 5

--- Week 1/Opdracht 5.sql
+++ Week 1/Opdracht 5.sql
@@ -1,3 +1,8 @@
 -- De accountants die bezig zijn met het vaststellen van de budgetten hebben het gemiddelde salaris van de
 -- medewerkers en het totaal van alle salarissen nodig. De resultaattabel moet twee kolommen bevatten
 -- op basis van een query. Geef de kolommen zinvolle namen.
+SELECT 
+  SUM(personeel.pers_salaris) as totaal_salaris, 
+  AVG(personeel.pers_salaris) as gemiddeld_salaris
+FROM 
+  personeel
