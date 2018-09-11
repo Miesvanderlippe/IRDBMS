16a4a81bdcf7c646f25b8bb1798e7a37929922ce
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 17:38:23 +0200

    Week 2 opdracht 5

--- Week2/Opdracht 5.sql
+++ Week2/Opdracht 5.sql
@@ -1,1 +1,17 @@
 -- De directie vraagt of de behandeling ‘Zuurstof’ uit het overzicht gehaald kan worden. Maak dit overzicht.
+
+SELECT
+  count(*) as aantal_per_verrichting,
+  v.verrichting_omschrijving
+FROM behandeling
+LEFT JOIN
+  verrichting v ON
+  behandeling.behandeling_verrichting_nr = v.verrichting_nr
+WHERE
+  v.verrichting_omschrijving != 'Zuurstof'
+GROUP BY
+  v.verrichting_nr
+HAVING
+  count(*) > 1
+ORDER BY
+  count(*), v.verrichting_omschrijving
