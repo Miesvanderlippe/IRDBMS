7a13932a37b36e37e83bc094a32ba1899fe969ef
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 17:38:11 +0200

    Week 2 opdracht 3

--- Week2/Opdracht 3.sql
+++ Week2/Opdracht 3.sql
@@ -1,1 +1,11 @@
 -- Het aantal behandelingen per verrichting. Toon de verrichting omschrijving.
+
+SELECT
+  count(*) as aantal_per_verrichting,
+  v.verrichting_omschrijving
+FROM behandeling
+LEFT JOIN
+  verrichting v ON
+  behandeling.behandeling_verrichting_nr = v.verrichting_nr
+GROUP BY
+  v.verrichting_nr
