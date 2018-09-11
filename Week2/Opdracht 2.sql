f0e29815e76bde0552dc5b75d3f50c5ea57df37e
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 17:38:06 +0200

    Week 2 opdracht 2

--- Week2/Opdracht 2.sql
+++ Week2/Opdracht 2.sql
@@ -1,2 +1,21 @@
 -- Het bed nummer zegt de centrale info niet zoveel. Breidt de query
 -- uit dat het kamernummer getoond wordt. Toon ook de kameromschrijving.
+SELECT
+  patient.patient_voornaam,
+  patient.patient_tussenvoegsel,
+  patient.patient_achternaam,
+  patient.patient_bed_nr,
+  b.bed_kamer_nr,
+  k.kamer_omschrijving
+FROM
+  patient
+LEFT JOIN
+  bed b ON
+  patient.patient_bed_nr = b.bed_nr
+LEFT JOIN
+  kamer k ON
+  b.bed_kamer_nr = k.kamer_nr
+WHERE
+  patient.patient_bed_nr NOTNULL
+ORDER BY
+  patient.patient_achternaam
