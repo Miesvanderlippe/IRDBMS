7f445c83eae475d0a22151dfc732f7da16da683d
Miesvanderlippe <miesvanderlippe@gmail.com>
2018-09-11 11:51:32 +0200

    Week 1 opdracht 6

--- Week 1/Opdracht 6.sql
+++ Week 1/Opdracht 6.sql
@@ -1,2 +1,9 @@
 -- Voor een voorschrift moet het ziekenhuis een overzicht leveren van het aantal verrichtingen per categorie.
 -- Toon het verrichting_cat_nr en het aantal verrichtingen.
+SELECT
+  verrichting.verrichting_cat_nr,
+  count(*) as aantal_verrichtingen
+FROM
+  verrichting
+GROUP BY
+  verrichting_cat_nr
