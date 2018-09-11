d459b236375009b1a28c85e80b7167e6f1e0b70b
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 17:37:55 +0200

    Week 2 opdracht 1

--- Week2/Opdracht 1.sql
+++ Week2/Opdracht 1.sql
@@ -1,2 +1,13 @@
 -- Een lijst op patient achternaam, tussenvoegsel en voornaam in welk bed deze patient zich bevindt
 -- (bed nummer). Sorteer op achternaam.
+
+SELECT
+  patient.patient_voornaam,
+  patient.patient_tussenvoegsel,
+  patient.patient_achternaam,
+  patient.patient_bed_nr
+FROM patient
+WHERE
+  patient.patient_bed_nr NOTNULL
+ORDER BY
+  patient.patient_achternaam
