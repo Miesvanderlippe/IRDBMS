faae7d6e3d6b11022806d698254e0ad3de1c6a42
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-11 17:38:16 +0200

    Week 2 opdracht 4

--- Week2/Opdracht 4.sql
+++ Week2/Opdracht 4.sql
@@ -1,3 +1,17 @@
 -- Breidt bovenstaande query uit door alle behandelingen die niet vaker dan 1 keer voorkomen
 -- niet te tonen. Sorteer op aantal behandelingen (de vaakst voorkomende bovenaan), gevolgd
 -- door verrichting omschrijving op alfabet
+
+SELECT
+  count(*) as aantal_per_verrichting,
+  v.verrichting_omschrijving
+FROM behandeling
+LEFT JOIN
+  verrichting v ON
+  behandeling.behandeling_verrichting_nr = v.verrichting_nr
+GROUP BY
+  v.verrichting_nr
+HAVING
+  count(*) > 1
+ORDER BY
+  count(*), v.verrichting_omschrijving
