278d7be8caef850beb083d182f5a8e26dc96e97c
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-12 11:09:07 +0200

    Week 2 opdracht 8

--- Week2/Opdracht 8.sql
+++ Week2/Opdracht 8.sql
@@ -1,1 +1,20 @@
 -- Maak een view die alleen de patiënten van de afdeling radiologie laat zien.
+-- Drop old view
+DROP VIEW IF EXISTS radiologiepatienten;
+
+-- Create new view
+CREATE VIEW radiologiepatienten AS
+  SELECT
+    patient.*,
+    v.verrichting_omschrijving
+  FROM
+    patient
+  LEFT JOIN
+    behandeling b on patient.patient_nr = b.behandeling_patient_nr
+  LEFT JOIN
+    verrichting v on b.behandeling_verrichting_nr = v.verrichting_nr
+  WHERE
+    v.verrichting_cat_nr = 'RAD';
+
+-- Select from view
+SELECT * from radiologiepatienten;
