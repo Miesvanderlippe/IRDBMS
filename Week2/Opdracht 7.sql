238b4760e4af6d1fc60bcd29ef27ccf43d17830f
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-12 11:08:55 +0200

    Week 2 opdracht 7

--- Week2/Opdracht 7.sql
+++ Week2/Opdracht 7.sql
@@ -1,1 +1,16 @@
 -- Maak een view die van een patiënt alleen de adresgegevens laat zien.
+
+-- Drop old view
+DROP VIEW IF EXISTS adresgegevens;
+
+-- Create new view
+CREATE VIEW adresgegevens AS
+  SELECT
+    patient.patient_nr,
+    patient.patient_adres,
+    patient.patient_postcode
+  FROM
+    patient;
+
+-- Select from view
+SELECT * from adresgegevens
