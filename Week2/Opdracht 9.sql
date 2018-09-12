08c0f6edc0f181129bc7d901c88f2021a923c972
Mies van der Lippe <miesvanderlippe@gmail.com>
2018-09-12 11:09:15 +0200

    Week 2 opdracht 9

--- Week2/Opdracht 9.sql
+++ Week2/Opdracht 9.sql
@@ -1,1 +1,13 @@
 -- Schrijf een view die alle verrichtingen van het laboratorium1 met 10% verhoogd.
+DROP VIEW IF EXISTS patientprijzen;
+
+-- Create new view
+CREATE VIEW patientprijzen AS
+  SELECT
+    verrichting.*,
+    round(verrichting.verrichting_rek_totaal * 1.1, 2) AS klant_prijs
+  FROM
+    verrichting;
+
+-- Select from view
+SELECT * from patientprijzen;
